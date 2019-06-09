5841c2d8b894f2aa9a1b716857099f8a56c81f26
Armat Davletalin <armat.net@gmail.com>
2019-06-10 01:25:31 +0600

    spaces added some new symbols added in properties

--- src/Solution.java
+++ src/Solution.java
@@ -7,7 +7,8 @@ public class Solution {
     public static void main(String[] args) {
         File propFile = new File("src\\resources\\Morse.properties");
         File source = new File("src\\resources\\sample.txt");
-        File output = new File("src\\resources\\output.txt");
+        File outputMorse = new File("src\\resources\\output.txt");
+        File outputText = new File("src\\resources\\toText.txt");
         Properties properties = new Properties();
         try {
             properties.load(new FileReader(propFile));
@@ -17,28 +18,25 @@ public class Solution {
         for (String letter : properties.stringPropertyNames()) {
             alphabet.put(letter, properties.getProperty(letter));
         }
-        String[] words = null;
+        String content = null;
         try {
-            String content = new Scanner(source).useDelimiter("\\Z").next();
-            words = content.split("\\s");
+            content = new Scanner(source).useDelimiter("\\Z").next();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        writeToFile(output, convertToMorse(words));
-        for (String s : convertMorseToEngFromFile(output)) {
+        writeToFile(outputMorse, convertToMorse(content));
+        List<String> textAgain = convertMorseToEngFromFile(outputMorse);
+        for (String s : textAgain) {
             System.out.print(s);
         }
-
-        System.out.println();
+        writeToFile(outputText, textAgain);
     }
 
-    private static ArrayList<String> convertToMorse(String[] words) {
+    private static ArrayList<String> convertToMorse(String words) {
         ArrayList<String> listOfWordsInMorse = new ArrayList<>();
-        for (String s : words) {
-            char[] charArrayOfWord = s.toCharArray();
-            for (int i = 0; i < charArrayOfWord.length; i++) {
-                listOfWordsInMorse.add(alphabet.get(String.valueOf(charArrayOfWord[i]).toUpperCase()) + " ");
-            }
+        char[] charArrayOfWord = words.toCharArray();
+        for (int i = 0; i < charArrayOfWord.length; i++) {
+            listOfWordsInMorse.add(alphabet.get(String.valueOf(charArrayOfWord[i]).toUpperCase()) + " ");
         }
         return listOfWordsInMorse;
     }
@@ -57,21 +55,21 @@ public class Solution {
     private static ArrayList<String> convertMorseToEngFromFile(File morseFile) {
         ArrayList<String> list = new ArrayList<>();
         try (Scanner scanner = new Scanner(morseFile)) {
-            String content = scanner.useDelimiter("\\Z").next();
-            String[] morseWords = content.split("\\s\\s");
-            StringBuilder sb = new StringBuilder();
+            String wholeMorseTextFromFile = scanner.useDelimiter("\\Z").next();
+            String[] morseWords = wholeMorseTextFromFile.split("\\s\\s");
+            StringBuilder morseLetter = new StringBuilder();
             for (int i = 0; i < morseWords.length; i++) {
-                String[] oneMorseWord = morseWords[i].split("\\s");
-                for (int j = 0; j < oneMorseWord.length; j++) {
-                    sb.append(oneMorseWord[j]);
+                String[] morseLetters = morseWords[i].split("\\s");
+                for (int j = 0; j < morseLetters.length; j++) {
+                    morseLetter.append(morseLetters[j]);
                 }
                 Set<Map.Entry<String, String>> entrySet = alphabet.entrySet();
-                String soughtValue = new String(sb);
+                String soughtValue = new String(morseLetter);
                 for (Map.Entry<String, String> pair : entrySet) {
                     if (soughtValue.equals(pair.getValue()))
                         list.add(pair.getKey());
                 }
-                sb = new StringBuilder();
+                morseLetter = new StringBuilder();
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
